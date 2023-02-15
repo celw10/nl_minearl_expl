a85c7c35ab9e24531d2a5ec086ae769db251e9de
celw10 <34209214+celw10@users.noreply.github.com>
2023-02-15 19:59:24 -0330

    Gridded images and more styling to Chris' Rocks

--- js/rocks.js
+++ js/rocks.js
@@ -31,7 +31,7 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 } 
 
-/*
+/* Auto Play version
 let slideIndex = 0;
 showSlides();
 
@@ -47,3 +47,45 @@ function showSlides() {
   setTimeout(showSlides, 10000); // Change image every 2 seconds
 } 
 */
+
+// ============================================================================
+// Image Grid
+// ============================================================================
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
